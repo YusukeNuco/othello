5f00c21e9d05b4fdcbb1117c34e0c7311d9682a4
YusukeNuco <yusuke_kikuchi@nuco.co.jp>
2025-04-15 14:22:05 +0900

    勝敗判定を実装

--- board.py
+++ board.py
@@ -1,13 +1,65 @@
+# 8 * 8 の盤面
+def show_board(board: list[list[str]]):
+    for row in board:
+        print(*row)
+
+
+# 黒石を置く
+def black(board, y, x):
+    if board[y][x] == '-':
+        board[y][x] = '○'
+    else:
+        print('置けません')
+
+
+# def black(board, y, x):
+#     while board[y][x] == '-':
+#         board[y][x] = '○'
+
+
+# 白石を置く
+def white(board, y, x):
+    if board[y][x] == '-':
+        board[y][x] = '●'
+    else:
+        print('置けません')
+
+
+# 勝敗判定
+def judgement(board: list[list[str]]):
+    black = 0
+    white = 0
+    for row in board:
+        black += row.count('○')
+        white += row.count('●')
+    if black < white:
+        print('white win!')
+        print(black)
+        print(white)
+    elif white < black:
+        print('black win!')
+        print(black)
+        print(white)
+    else:
+        print('draw')
+        print(black)
+        print(white)
+
+
 def main():
-    y = int(input())
-    x = int(input())
-    row_lis = ['-', '-', '-', '-', '-', '-', '-', '-',]
     board_rc = []
     for _ in range(8):
         board_rc.append(['-', '-', '-', '-', '-', '-', '-', '-',])
-    board_rc[y][x] = '⚪️'
-    print(*board_rc, sep = '\n')
-
+    board_rc[3][3] = '●'
+    board_rc[4][4] = '●'
+    board_rc[4][3] = '○'
+    board_rc[3][4] = '○'
+    print("black")
+    black(board=board_rc, y=int(input()), x=int(input()))
+    print("white")
+    white(board=board_rc, y=int(input()), x=int(input()))
+    show_board(board=board_rc)
+    judgement(board=board_rc)
 
 
 if __name__ == '__main__':
--- function.py
+++ function.py
@@ -6,27 +6,50 @@ def show_board(board: list[list[str]]):
 
 # 黒石を置く
 def black(board, y, x):
-    board[y][x] = '●'
+    if board[y][x] == '-':
+        board[y][x] = '○'
+    else:
+        print('置けません')
 
 
 # 白石を置く
 def white(board, y, x):
-    board[y][x] = '○'
+    if board[y][x] == '-':
+        board[y][x] = '●'
+    else:
+        print('置けません')
+
+
+# 勝敗判定
+def judgement(board: list[list[str]]):
+    black = 0
+    white = 0
+    for row in board:
+        black += row.count('○')
+        white += row.count('●')
+    if black < white:
+        print('white win!')
+    elif white < black:
+        print('black win!')
+    else:
+        print('draw')
 
 
 def main():
     board_rc = []
     for _ in range(8):
         board_rc.append(['-', '-', '-', '-', '-', '-', '-', '-',])
-    black(board=board_rc, y=int(input()), x=int(input()))
-    white(board=board_rc, y=int(input()), x=int(input()))
     board_rc[3][3] = '●'
     board_rc[4][4] = '●'
     board_rc[4][3] = '○'
     board_rc[3][4] = '○'
+    print("black")
+    black(board=board_rc, y=int(input()), x=int(input()))
+    print("white")
+    white(board=board_rc, y=int(input()), x=int(input()))
     show_board(board=board_rc)
+    judgement(board=board_rc)
 
 
 if __name__ == '__main__':
     main()
-
